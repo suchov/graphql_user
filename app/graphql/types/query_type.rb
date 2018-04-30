c9681680ed0a480d73787dd89b13f55834da17a0
suchov <artem.suchov@gmail.com>
2018-04-30 12:44:58 +0300

    generate the models and create the user with all the fields in the user_type and represent it in query_type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,25 @@
 Types::QueryType = GraphQL::ObjectType.define do
-  name "Query"
+  name 'Query'
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
 
   # TODO: remove me
   field :testField, types.String do
-    description "An example field added by the generator"
+    description 'An example field added by the generator'
     resolve ->(obj, args, ctx) {
-      "Hello World!"
+      'The data is sanding, all is tested, yep!'
+    }
+  end
+  # TODO: fields that mast exists:
+  # User (first name, last name, street, number, city, postcode, country), Post(user_id, body) and Comment(user_id, body)
+  # Add fields in User to include all Posts, and in Post to include all Comments
+  # User should have a custom field to return the full address and the full name.
+  # How did you write the method to return the full address of a user?
+  field :user, Types::UserType do
+    argument :id, types.ID, 'The id of the user to retreve'
+    description 'One user'
+    resolve ->(obj, args, ctx) {
+      User.where(id: args[:id]).first
     }
   end
 end
---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,13 @@
+Types::UserType = GraphQL::ObjectType.define do
+  name 'UserType'
+
+  field :first_name, types.String, description: 'The first name of the user'
+  # last name, street, number, city, postcode, country
+  field :last_name, types.String, description: 'The last name of the user'
+  field :street, types.String, description: 'The street on which user leaves'
+  field :number, types.Int, description: 'The number of the building'
+  field :city, types.String, description: 'The city where all this happens'
+  field :postcode, types.Int, description: 'The postal code of all that'
+  field :country, types.String, description: 'In what country all that happens'
+  field :id, !types.ID # !not allow the field to be nil - always must have value
+end
